794c5c6968ee5b02018da13dba0f2a7208e0678d
felipeUrra <felipeurrarivadeneira@gmail.com>
2025-12-01 01:25:00 +0200

    testing

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import tracker
 
 tracker = tracker.Tracker()
-tracker.addExpense(20, 'caramelos')
-tracker.addExpense(4, 'chicle')
-print(tracker.expenses)
+tracker.addExpense('caramelos', 20)
+tracker.addExpense('chicle', 4)
 
 tracker.deleteExpense(1)
-print(tracker.expenses)
 
-tracker.addExpense(23, 'agua')
-print(tracker.expenses)
+tracker.addExpense('agua', 13)
+
+tracker.listExpenses()
+
+tracker.updateExpense(1, 'refresco', 20)
+tracker.listExpenses()
