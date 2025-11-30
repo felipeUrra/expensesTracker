cb87371d2a5efabfaa34a9792e6f06a1001bccdc
felipeUrra <felipeurrarivadeneira@gmail.com>
2025-12-01 01:25:43 +0200

    add getInfo()

--- expense.py
+++ expense.py
@@ -3,9 +3,12 @@ import datetime
 class Expense:
     nextId = 1
 
-    def __init__(self, amount, description, date):
+    def __init__(self, description, amount, date):
         self.amount = amount
         self.description = description
-        self.date = date
+        self.date = date.strftime("%Y-%m-%d")
         self.id = Expense.nextId
         Expense.nextId += 1
+
+    def getInfo(self):
+        return '{} {} {} {}'.format(self.id, self.date, self.description, self.amount)
