6ebf3c3dd04313bf543c3f4f180c8b128b7ba09e
felipeUrra <felipeurrarivadeneira@gmail.com>
2025-12-01 01:26:34 +0200

    add updateExpense() and listExpenses()

--- tracker.py
+++ tracker.py
@@ -6,8 +6,8 @@ class Tracker:
         self.commands = {'add', 'update', 'delete', 'list', 'summary'}
     
     # Commands
-    def addExpense(self, amount, description): 
-        newExpense = expense.Expense(amount, description, expense.datetime.datetime.now())
+    def addExpense(self, description, amount): 
+        newExpense = expense.Expense(description, amount, expense.datetime.datetime.now())
         self.expenses.append(newExpense)
 
         print("Expense added successfully (ID: " + str(newExpense.id) + ")")
@@ -19,7 +19,28 @@ class Tracker:
         else:
             self.expenses.pop(index)
             print("Expense deleted successfully!")
-    
+
+    def updateExpense(self, id, description, amount):
+        index = self.getExpenseIndexById(id)
+        if index == None:
+            print("No expenses with that ID!")
+        else:
+            self.expenses[index].description = description
+            self.expenses[index].amount = amount
+            print("Expense updated successfully!")
+
+    def listExpenses(self):
+        data = []
+        for expense in self.expenses:
+            data.append(list(expense.getInfo().split(' ')))
+        
+        header = ["ID", "Date", "Description", "Amount"]
+
+        print(f"{header[0]:<5} {header[1]:<15} {header[2]:<20} {header[3]:<10}")
+        print('-' * 50)
+
+        for info in data:
+            print(f"{info[0]:<5} {info[1]:<15} {info[2]:<20} {info[3]:<10}")
 
 
     # Utils (maybe its going to be in a specific class)
